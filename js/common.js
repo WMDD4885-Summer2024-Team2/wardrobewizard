eb7f2e59463c8dee4f3849e777ab16ea724a4b46
Uma Maheswara Rao <uvoleti00@mylangara.ca>
2024-06-27 17:29:19 -0700

    Color Matching Changes

---
+++ js/common.js
@@ -0,0 +1,116 @@
+
+//Function to convert hex color to hsl color
+export const hexToHsl = function(hex) {
+    // Remove the hash at the start if it's there
+    hex = hex.replace(/^#/, '');
+
+    // Parse the r, g, b values
+    let r = parseInt(hex.substring(0, 2), 16) / 255;
+    let g = parseInt(hex.substring(2, 4), 16) / 255;
+    let b = parseInt(hex.substring(4, 6), 16) / 255;
+
+    // Find the max and min values to get the lightness
+    let max = Math.max(r, g, b);
+    let min = Math.min(r, g, b);
+    let h, s, l = (max + min) / 2;
+
+    if (max === min) {
+        h = s = 0; // Achromatic
+    } else {
+        let d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch (max) {
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+        h /= 6;
+    }
+
+    h = Math.round(h * 360);
+    s = Math.round(s * 100);
+    l = Math.round(l * 100);
+
+    return [h,s,l];
+}
+
+const allPages = document.querySelectorAll('div.page');
+
+//SPA (Single Page Application)
+export const navigateToPage = function() {
+  const pageId = location.hash ? location.hash : '#page1';
+  for (let page of allPages) {
+    if (pageId === '#' + page.id) {
+      page.style.display = 'grid';
+    } else {
+      page.style.display = 'none';
+    }
+  }
+  return;
+}
+
+
+export const base64ToBlob = function(base64, mimeType) {
+  // Decode the base64 string to a binary string
+  let binaryString = atob(base64.split(',')[1]);
+
+  // Create a byte array with the same length as the binary string
+  let byteArray = new Uint8Array(binaryString.length);
+
+  // Fill the byte array with the binary string's character codes
+  for (let i = 0; i < binaryString.length; i++) {
+      byteArray[i] = binaryString.charCodeAt(i);
+  }
+
+  // Create a Blob from the byte array
+  let blob = new Blob([byteArray], { type: mimeType });
+
+  return blob;
+}
+
+
+export const startCamera = (video)=>{
+  if(video && navigator.mediaDevices && navigator.mediaDevices.getUserMedia){
+
+    const mediaPromise=navigator.mediaDevices.getUserMedia({video:true});
+    mediaPromise.then((stream)=>{
+      video.srcObject = stream;
+    }).catch((error) =>{
+      console.error(error);
+    });
+  }
+}
+
+export const stopCamera = (video) =>{
+  if(video && video.srcObject){
+    const tracks=video.srcObject.getTracks();
+    tracks.forEach((track) => track.stop());
+  }
+ 
+}
+ 
+
+// Reusable fetch utility function
+export const fetchData = async (url, method, data = null) => {
+  const options = {
+    method: method,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: data ? JSON.stringify(data) : null,
+  };
+
+  try {
+    const response = await fetch(url, options);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return await response.text();
+  } catch (error) {
+    console.error("Error during fetch:", error);
+    throw error;
+  }
+};
+
+
+
---
+++ js/outfitsearch.js
@@ -0,0 +1,32 @@
+import { fetchData } from "./common.js";
+import config from '../resources/config.json' with { type: 'json' };
+
+export const genreateOutfit = async (topOutfit = [], bottomOutfit = [], searchParams) => {
+
+    let matchingOutfit=[];
+
+    
+    if(topOutfit.length === 0 || bottomOutfit.length === 0 )
+        return matchingOutfit;
+
+    if(searchParams === 'Auto'){
+
+        for(const outfit of bottomOutfit ){
+           // const topoutfit=topOutfit[Math.floor(Math.random() * topOutfit.length)];
+            for(const topoutfit of topOutfit){
+                const result = await fetchData(config.colormatchURL,'POST',{ 'top_hsv': topoutfit.color,'bot_hsv':outfit.color});
+                if(result && result.length>0 ){
+                    matchingOutfit.push([topoutfit,outfit]);
+                }
+            }
+           
+
+                
+               
+        }
+        
+    }
+
+   
+    return matchingOutfit;
+};
