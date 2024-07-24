73b1bd5bf09e7a9c22c5f2775e3dc6b880b85eae
Amneesh <amneeshsingh5@gmail.com>
2024-07-23 18:18:21 -0700

    header and homepage changes
    header and homepage changes

--- js/header.js
+++ js/header.js
@@ -2,11 +2,35 @@ import { userSignOut } from "./common.js"
 
 export const init = () => {  
    signOutButton.addEventListener("click", userSignOut);
+   signOutButton2.addEventListener("click", userSignOut);
 
    closeBtn.addEventListener('click',()=>{
       uploadDialog.close();
      });
   
-   
+     var headerMenu = document.getElementById('header-menu');
+
+     function openMenu() {
+       headerMenu.setAttribute('class', 'bloom-mobile-header nav-visible');
+     }
+     function closeMenu() {
+       headerMenu.removeAttribute('class', ' bloom-mobile-header nav-visible');
+       headerMenu.setAttribute('class', ' bloom-mobile-header');
+     }
+     
+     openNavButton.addEventListener("click", function (event) {
+       openMenu();
+     })
+     
+     closeNavButton.addEventListener("click", function (event) {
+       closeMenu();
+     })
+     var mq = window.matchMedia("(min-width: 700px)");
+     mq.addEventListener("change", function() {
+       if (mq.matches) {
+         closeMenu();
+       }
+     });
+     
 }
 
