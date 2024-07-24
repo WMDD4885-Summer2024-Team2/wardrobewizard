891428eb4901e075d5dae8d55eb2b7ca90461331
Amneesh <amneeshsingh5@gmail.com>
2024-07-23 21:31:40 -0700

    changes modal box
    changes modal box

--- js/viewwardrobe.js
+++ js/viewwardrobe.js
@@ -1,46 +1,46 @@
-// import { getOutfitsByCategory } from "./common.js";
+import { getOutfitsByCategory } from "./common.js";
 
-// export const init = () => {
-//   showOutfit();
-//   viewwardrobe.addEventListener("click", showOutfit);
+export const init = () => {
+  showOutfit();
+  viewwardrobe.addEventListener("click", showOutfit);
 
-//   outfitupload.addEventListener('click', ()=>{
-//     window.location.href = '#uploadoutfitmycloset';
-//   });
-// };
+  outfitupload.addEventListener('click', ()=>{
+    window.location.href = '#uploadoutfitmycloset';
+  });
+};
 
-// const showOutfit = () => {
-//   renderdata("top", tops);
-//   renderdata("bottom", bottoms);
-//   renderdata("dress", dresses);
-// };
+const showOutfit = () => {
+  renderdata("top", tops);
+  renderdata("bottom", bottoms);
+  renderdata("dress", dresses);
+};
 
-// const renderdata = (catgeory, element) => {
-
-
-//   let outfits = getOutfitsByCategory(catgeory);
-
-//   if(outfits.length === 0){
-//     element.innerHTML +=`Sorry, There are no ${catgeory}'s available in your wardrobe to display.`;
-//     return;
-//   }
-
-//   outfits.forEach((outfit) => {
-//     let tags = "";
+const renderdata = (catgeory, element) => {
 
 
-//     outfit.tags.forEach((tag) => {
-//       tags += `<span class='tag-info'>${tag}</span>`;
-//     });
+  let outfits = getOutfitsByCategory(catgeory);
 
-//     element.innerHTML += `<div class='card flex-grow'>
-//         <div class='card-body'>
-//           <div><img src='${outfit.downloadURL}'></div>
+  if(outfits.length === 0){
+    element.innerHTML +=`Sorry, There are no ${catgeory}'s available in your wardrobe to display.`;
+    return;
+  }
+
+  outfits.forEach((outfit) => {
+    let tags = "";
+
+
+    outfit.tags.forEach((tag) => {
+      tags += `<span class='tag-info'>${tag}</span>`;
+    });
+
+    element.innerHTML += `<div class='card flex-grow'>
+        <div class='card-body'>
+          <div><img src='${outfit.downloadURL}'></div>
         
-//         </div>
-//         <div class='card-footer'>
-//           ${tags}
-//         </div>
-//       </div>`;
-//   });
-// };
+        </div>
+        <div class='card-footer'>
+          ${tags}
+        </div>
+      </div>`;
+  });
+};
